1a369e6bf396c88cfbc55867e7599c9ad9e70672
hbbymth42 <hobbymath42@gmail.com>
2023-12-10 16:48:47 +1100

    Completed Lesson 15

--- README.md
+++ README.md
@@ -16,7 +16,7 @@ ___
 - [x] Lesson 12: Order of execution of a Query
 - [x] Lesson 13: Inserting rows
 - [x] Lesson 14: Updating rows
-- [ ] Lesson 15: Delecting rows
+- [x] Lesson 15: Delecting rows
 - [ ] Lesson 16: Creating tables
 - [ ] Lesson 17: Altering tables
 - [ ] Lesson 18: Dropping tables
---
+++ lesson_15.sql
@@ -0,0 +1,11 @@
+--- Deleting rows ---
+
+
+-- This database is getting too big, lets remove all movies that were released before 2005.
+DELETE FROM movies
+WHERE year < 2005;
+
+
+-- Andrew Stanton has also left the studio, so please remove all movies directed by him.
+DELETE FROM movies
+WHERE director LIKE "Andrew Stanton";
